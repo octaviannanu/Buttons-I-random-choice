b3748d70d61cc872dcff6e203e6c524560cee2a4
octaviannanu <100152463+octaviannanu@users.noreply.github.com>
2022-03-13 14:23:59 +0200

    Update Button.js

--- Button.js
+++ Button.js
@@ -3,7 +3,7 @@ function play() {
   good_number = Math.floor(Math.random()*(3-1+1))+1;
 }
 function winner(button_number, good_number) {
-  if (good_number === button-number) {
+  if (button_number === good-number) {
     alert('Yes! That is a good choice!')
   } else {
     alert('Ouch! Stop poking me! Bad choice!')
